cdecacfeed2fd2156a36d8c77d3fe343fae0c397
chaimaa <cm3568@stern.nyu.edu>
2019-06-15 18:42:39 -0400

    Final Commit

--- groceries.py
+++ groceries.py
@@ -80,7 +80,10 @@ unique_departments.sort()
 
 for d in unique_departments:
     matching_products = [item for item in products if item["department"] == d]
-
     matching_products_count = len(matching_products)
-    print(d.title() + " (" + str(matching_products_count) + " products)")
+    if matching_products_count > 1:
+        label = "products"
+    else:
+        label = "product"
+    print(d.title() + " (" + str(matching_products_count) + " " + label + ")")
 
