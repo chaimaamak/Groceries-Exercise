cdb7c3e9c1ea53296099267c2b9c4578ffce441f
chaimaa <cm3568@stern.nyu.edu>
2019-06-15 18:32:48 -0400

    Identify Matching Products
    Used list comprehension, identified matching products, listed them, and displayed them

--- groceries.py
+++ groceries.py
@@ -79,5 +79,8 @@ print("-----------")
 unique_departments.sort()
 
 for d in unique_departments:
-    print(d.title())
+    matching_products = [item for item in products if item["department"] == d]
+
+    matching_products_count = len(matching_products)
+    print(d.title() + " (" + str(matching_products_count) + " products)")
 
